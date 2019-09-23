5248cd5de5d44920d2578defb89c0bc1c1b426eb
Sarah Loach <Sarah@Sarahs-fat-top.attlocal.net>
2019-09-23 00:24:40 -0500

    added JS file to hold questions contents

---
+++ store.js
@@ -0,0 +1,53 @@
+const STORE = [
+    {
+        question: 'Red, Blue and Yellow are:',
+        possibleAnswers: [
+            'Tertiary  Colors',
+            'Primary Colors',
+            'Monochromatic Colors',
+            'Complementary Colors'
+        ],
+        correctAnswer: 'Primary Colors',
+        bgColor: 'linear-gradient(to bottom right, red, blue, yellow)'
+    },
+    {
+        question: 'Purple, Green and Orange are:',
+        possibleAnswers: [
+            'Tertiary  Colors',
+            'Primary Colors',
+            'Secondary Colors',
+            'Complementary Colors'
+        ],
+        correctAnswer: 'Secondary Colors'
+    },
+    {
+        question: 'What are two colors that are directly across from each other on the color wheel called?',
+        possibleAnswers: [
+            'Analogous',
+            'Complementary',
+            'Monochromatic',
+            'Complementary'
+        ],
+        correctAnswer: 'Complementary'
+    },
+    {
+        question: 'With which colors do you make the color green?',
+        possibleAnswers: [
+            'Red & Blue',
+            'Yellow & Blue',
+            'Orange & Blue',
+            'Purple & Yellow'
+        ],
+        correctAnswer: 'Yellow & Blue'
+    },
+    {
+        question: 'Which of the following is a tertiary color?',
+        possibleAnswers: [
+            'Purple',
+            'Orange',
+            'Green',
+            'Brown'
+        ],
+        correctAnswer: 'Brown'
+    }
+]
